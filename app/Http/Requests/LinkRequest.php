60adfbcb90f460cedd1a54338983631f62a065b4
vikzh <vikt.anmail@gmail.com>
2019-06-15 10:42:06 +0300

    validation added

--- app/Http/Requests/LinkRequest.php
+++ app/Http/Requests/LinkRequest.php
@@ -7,16 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 class LinkRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
@@ -24,7 +14,7 @@ class LinkRequest extends FormRequest
     public function rules()
     {
         return [
-            'url' => 'required|url'
+            'url' => 'url|required|min:3'
         ];
     }
 }
